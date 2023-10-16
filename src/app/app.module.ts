b23a2ec3180894009a645e6918d0c39b914e0983
George Amuzu <gnamuzu@gmail.com>
2023-10-16 16:53:28 +0100

    added search section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './header/profile/profile.component';
+import { SearchComponent } from './header/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
